25bb12be859fbe19ebca3571af0c398a9a0bb132
Jackeagle <jackeagle102@gmail.com>
2014-09-18 22:24:24 +0530

    Initial AOGP Vendor Setup

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,14 +1,14 @@
 # Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common.mk)
+$(call inherit-product, vendor/aogp/config/common.mk)
 
 # Bring in all video files
 $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 
 # Include CM audio files
-include vendor/cm/config/cm_audio.mk
+include vendor/aogp/config/aogp_audio.mk
 
 # Include CM LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/aogp/overlay/dictionaries
 
 # Optional CM packages
 PRODUCT_PACKAGES += \
