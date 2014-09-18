e560f85039fcbdcdd76aa66449bd61d7567e18dd
Jackeagle <jackeagle102@gmail.com>
2014-09-18 22:30:33 +0530

    Initial AOGP Vendor Setup

--- config/common_versions.mk
+++ config/common_versions.mk
@@ -1,7 +1,7 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-DATE = $(shell vendor/aicp/tools/getdate)
+DATE = $(shell vendor/aogp/tools/getdate)
 AOGP_BRANCH=kitkat
 
 # AOGP RELEASE VERSION
