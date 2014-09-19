dd63afe24e39f6d81d1a7d65b9b2e8295ed53207
Jackeagle <jackeagle102@gmail.com>
2014-09-19 11:29:33 +0530

    Adding Devices

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,2 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-11.0 | awk {'print $1'})
-do
-    add_lunch_combo $combo
-done
+add_lunch_combo aogp_hammerhead-userdebug
+add_lunch_combo aogp_i9300-userdebug
